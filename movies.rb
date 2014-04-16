3ba2ceea940a258b6c679b035e92d0ea767bccd6
tigarcia <tim.m.garcia@gmail.com>
2014-04-16 16:19:44 -0700

    Adding a possible movie solution

--- movies.rb
+++ movies.rb
@@ -5,11 +5,53 @@ require 'typhoeus'
 require 'json'
 
 get '/' do
+  html = %q(
+  <html><head><title>Movie Search</title></head><body>
+  <h1>Find a Movie!</h1>
+  <form accept-charset="UTF-8" action="/results" method="get">
+    <label for="movie">Search for:</label>
+    <input id="movie" name="movie" type="text" />
+    <input name="commit" type="submit" value="Search" />
+  </form></body></html>
+  )
 end
 
 get '/results' do
+  search_str = params[:movie]
+
+  # Make a request to the omdb api here!
+  response = Typhoeus.get("www.omdbapi.com", :params => {s: search_str})
+  movies = JSON.parse(response.body)["Search"]
+
+  # Modify the html output so that a list of movies is provided.
+  html_str = "<html><head><title>Movie Search Results</title></head><body><h1>Movie Results</h1>\n<ul>"
+  if movies.nil?
+    html_str +=  "<h2>That movie doesn't exist!</h2> <img src = 'http://i.imgur.com/QCY6iG5.jpg' /> "
+  else
+    sorted_movies = movies.sort_by { |x| x["Year"]}.reverse
+    sorted_movies.each do |x|
+      html_str += "<li><a href=poster/#{x['imdbID']}>#{x["Title"]}, #{x["Year"]}</a></li>"
+    end
+  end
+
+  html_str += "</ul><br /><a href='/'>New Search</a></body></html>"
 end
 
-# TODO: Add another get here for the poster url.  The path for the poster
-# should look like this example '/poster/tt2724064'
+post '/test' do
+  search_str = params[:movie]
+  response = Typhoeus.get("www.omdbapi.com", :params => {s: search_str})
+  movies = JSON.parse(response.body)
+end
+
+get '/poster/:imdb' do |imdb_id|
+  # Make another api call here to get the url of the poster.
+  response = Typhoeus.get("www.omdbapi.com", :params => {i: imdb_id})
+  poster = JSON.parse(response.body)
+
+  html_str = "<html><head><title>Movie Poster</title></head><body><h1>Movie Poster</h1>\n"
+  html_str += "<h3>#{poster["Title"]}</h3>"
+  html_str += "<object data=#{poster["Poster"]}><img src='http://i.imgur.com/QCY6iG5.jpg' alt = 'Movie Poster Image' ></object>"
+  html_str += '<br /><a href="/">New Search</a></body></html>'
+
+end
 
