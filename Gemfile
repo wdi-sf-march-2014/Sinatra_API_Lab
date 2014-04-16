1616f43c3489c5ca1a4ead9d11f094110890fdb2
tigarcia <tim.m.garcia@gmail.com>
2014-04-16 10:37:37 -0700

    Initial setup for the lab.  Added Gem and a stubbed out movies app

---
+++ Gemfile
@@ -0,0 +1,10 @@
+source "http://rubygems.org"
+
+gem "sinatra"
+gem "sinatra-contrib"
+gem "typhoeus"
+gem "json"
+
+gem "rspec"
+gem "pry"
+gem "capybara"
---
+++ movies.rb
@@ -0,0 +1,15 @@
+require 'bundler/setup'
+require 'sinatra'
+require 'sinatra/reloader'
+require 'typhoeus'
+require 'json'
+
+get '/' do
+end
+
+get '/result' do
+end
+
+post '/test' do
+end
+
