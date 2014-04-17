2113d81f9d11ef7542c5b3239170e35228f1b8f6
antmachine <anthonysallows@gmail.com>
2014-04-17 15:38:17 -0700

    Ability to search and get a list of links

--- movies.rb
+++ movies.rb
@@ -4,12 +4,38 @@ require 'sinatra/reloader'
 require 'typhoeus'
 require 'json'
 
+
+
 get '/' do
+	'<h1> Search for your favorite movies! </h1>
+  <form action="/results" method="get">
+  <input type="text" input name="movie" placeholder="Movie Title"><br>
+  <input type="submit" value="Submit">
+	</form>'
 end
+
+
+
 
 get '/results' do
-end
 
+response = Typhoeus.get("http://www.omdbapi.com/", :params => {:s => params["movie"]})
+parsed = JSON.parse(response.body)
+
+str = ""
+
+parsed['Search'].each do |movie|
+	#try putting the full link to poster rather than the imdbID
+str += "<br><a href=poster/#{movie["imdbID"]}> #{movie["Title"]} - #{movie["Year"]} </a><br>"
+	end
+	str
+end
 # TODO: Add another get here for the poster url.  The path for the poster
 # should look like this example '/poster/tt2724064'
+
+get '/poster/:imdbID' do
+	response = Typhoeus.get("http://www.omdbapi.com/", :params => {:s => params["imdbID"]})
+	posters = JSON.parse(response.body)
+	"<img src = #{posters["Poster"]}/>"
+end
 
