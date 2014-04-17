63ea0542cf0287117dd5dd2b827ff80999b445ec
Githubshanna <shannahutch@gmail.com>
2014-04-17 10:04:46 -0700

    Sinatra APP lab

--- movies.rb
+++ movies.rb
@@ -4,12 +4,85 @@ require 'sinatra/reloader'
 require 'typhoeus'
 require 'json'
 
+
 get '/' do
+	# "{params[:movie]}"
+	html_str = 
+<<-eos
+	<html>
+	<body>
+	<header>
+	<h1>Enter a Movie title here to Search<h1>
+	<header>
+		<form method="get" action="/results">
+
+		<input type='text' name='movie'>
+		<input type='submit'>
+
+		</form>
+
+		<body>
+		</html>
+eos
+
 end
+	
+
+
 
 get '/results' do
+
+	search = params[:movie]
+
+	if search == "" || search == nil
+		return "This movie does not exist"
+	end
+	
+	results = Typhoeus.get("www.omdbapi.com/?s=#{search}")
+	imdb_data = JSON.parse(results.body) #{search}))
+	
+	imdb_data.inspect
+
+	# if results != do
+	# 	"Movie Does not Exist"
+	
+
+html_str =
+<<-eos
+
+<html>
+<head>
+</head>
+<body>	
+eos
+	imdb_data["Search"].each do |h| 
+		html_str += "<a href='/poster/#{h["imdbID"]}'>< #{h["Title"]} - #{h["Year"]}}><br></br>a>"
+	end
+
+	html_str += "</body></html>"
+
+	
+
 end
 
-# TODO: Add another get here for the poster url.  The path for the poster
+
+
+get '/poster/:imdbID' do
+	imdb_id = params[:imdbID]
+	new_results = Typhoeus.get("www.omdbapi.com/?i=#{imdb_id}")
+	imdb_id= JSON.parse(new_results.body)
+	imdb_id.inspect
+
+	html_str = 
+<<-eos
+<html>
+<head>
+</head>
+<body>
+eos
+html_str += "<img src='#{imdb_id["Poster"]}'/>"
+
+end
 # should look like this example '/poster/tt2724064'
 
+#imdbid[Poster].value
