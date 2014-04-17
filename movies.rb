2044964efdeefb32ca5a345daea3f321ec44b07f
flannimal <flanaganm09@gmail.com>
2014-04-16 19:13:23 -0700

    initial commit

--- movies.rb
+++ movies.rb
@@ -4,12 +4,32 @@ require 'sinatra/reloader'
 require 'typhoeus'
 require 'json'
 
-get '/' do
-end
+# s (NEW!)	 string (optional)	 title of a movie to search for
+# i	 string (optional)	 a valid IMDb movie id
+#get request will be to own server / action = result
+
+get '/' do <<EOS
+	  <h1> Movie search knock pants off </h1> 
+		<form action="/results" method="get" />
+		<input type="text" name="movie" placeholder="E.g. Police Academy" />
+		<input type="submit"> 
+EOS
+	end
+			# <input type="text" name="year" value="2006" />
 
 get '/results' do
+	res=Typhoeus.get("www.omdbapi.com/", :params => { :s => params["movie"] })  # :y => params["year"]
+  json = JSON.parse(res.body) 
+  str = ""
+  p json.to_s
+ 	# json["Search"].each do |movie|
+ 	# 	str += "<br><a href> #{movie["Title"]} - #{movie["Year"]} </a><br>"
+ 	# end
+ 	# str
+
 end
 
-# TODO: Add another get here for the poster url.  The path for the poster
-# should look like this example '/poster/tt2724064'
+get '/poster/[imdb_id]' do
+	res=Typhoeus.get("www.omdbapi.com/", :params => { :t => params["movie"] })
+end
 
