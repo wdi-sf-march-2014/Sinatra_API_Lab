439efe7b923896fea63c20593080a079e286851e
Henry Huang <hhuang2088@gmail.com>
2014-04-16 20:56:41 -0700

    info needs presentation

--- movies.rb
+++ movies.rb
@@ -5,11 +5,28 @@ require 'typhoeus'
 require 'json'
 
 get '/' do
+	<<-eos
+	<form action="/results" method="get">
+	<input type='text' name="movie" >
+	<button>Submit</button>
+	</form>
+	eos
 end
 
+
 get '/results' do
+	arr = []
+	search_string = params[:movie]
+	search = Typhoeus.get("http://www.omdbapi.com/", :params => {:s => search_string})
+	result = JSON.parse(search.body)
+	result["Search"].to_s
+	result["Search"].each do |instance|
+		arr << "#{instance["Title"]} #{instance["Year"]}"
+	end
+	arr.join(" ")
 end
 
 # TODO: Add another get here for the poster url.  The path for the poster
 # should look like this example '/poster/tt2724064'
-
+# get '/poster/'
+# end
