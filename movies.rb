410f7bb67cb76d06b996958c2af85b1796405937
flannimal <flanaganm09@gmail.com>
2014-04-16 19:49:27 -0700

    passes rspec, no bonus

--- movies.rb
+++ movies.rb
@@ -4,32 +4,35 @@ require 'sinatra/reloader'
 require 'typhoeus'
 require 'json'
 
-# s (NEW!)	 string (optional)	 title of a movie to search for
-# i	 string (optional)	 a valid IMDb movie id
-#get request will be to own server / action = result
-
 get '/' do <<EOS
 	  <h1> Movie search knock pants off </h1> 
 		<form action="/results" method="get" />
 		<input type="text" name="movie" placeholder="E.g. Police Academy" />
 		<input type="submit"> 
 EOS
-	end
-			# <input type="text" name="year" value="2006" />
+end
 
 get '/results' do
+	if params.keys.length == 0 or params.keys.length == nil
+		return "Please get a clue."
+	end
+
 	res=Typhoeus.get("www.omdbapi.com/", :params => { :s => params["movie"] })  # :y => params["year"]
   json = JSON.parse(res.body) 
   str = ""
-  p json.to_s
- 	# json["Search"].each do |movie|
- 	# 	str += "<br><a href> #{movie["Title"]} - #{movie["Year"]} </a><br>"
- 	# end
- 	# str
+  # p json.to_s
+ 	
+ 	json["Search"].each do |movie|
+ 		str += "<br><a href=poster/#{movie["imdbID"]}> #{movie["Title"]} - #{movie["Year"]} </a><br>"
+ 	end
+ 	str
 
 end
 
-get '/poster/[imdb_id]' do
-	res=Typhoeus.get("www.omdbapi.com/", :params => { :t => params["movie"] })
+get '/poster/:imdbID' do
+	res=Typhoeus.get("www.omdbapi.com/", :params => { :i => params["imdbID"] })
+	pic = JSON.parse(res.body) 
+	"<img src = #{pic["Poster"]} />"
+
 end
 
